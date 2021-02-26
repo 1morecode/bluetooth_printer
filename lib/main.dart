26a80894d643c68218963a34374da0dacc677807
Rahul Yadav <1morecode@gmail.com>
2021-02-26 16:54:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:blue/printer.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:oktoast/oktoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OKToast(
+      child: MaterialApp(
+        title: 'Bluetooth demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
